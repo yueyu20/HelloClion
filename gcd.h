40e9366f794d402d8539084e133d6dc9f35a46f3
Yue Yu <yueyu1015@gmail.com>
2018-09-13 10:57:57 -0700

    GCD method

---
+++ gcd.h
@@ -0,0 +1,15 @@
+//
+// Created by 虞跃 on 2018-09-13.
+//
+
+#ifndef HELLOCLION_GCD_H
+#define HELLOCLION_GCD_H
+
+//Greatest Common Denominator
+//PRE: a is a positive integer
+//PRE: b is a positive integer
+//POST: a and b are unchanged
+//RETURN: the greatest common denominator of a and b.
+int gcd(const int a, const int b);
+
+#endif //HELLOCLION_GCD_H
--- main.cpp
+++ main.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
+#include "gcd.h"
 
-//Greatest Common Denominator
-//PRE: a is a positive integer
-//PRE: b is a positive integer
-//POST: a and b are unchanged
-//RETURN: the greatest common denominator of a and b.
-int gcd(const int a, const int b);
+
 
 int main() {
-    int n = gcd(44,66);
+    int n = gcd(25,10);
     std::cout << "Hello, Clion!   hello" << std::endl;
     std::cout << n << std::endl;
     return 0;
